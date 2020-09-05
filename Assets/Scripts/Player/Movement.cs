87c16d3c13a9ea6432f3ad35a781a1b128224562
towergame <h@h.h>
2020-09-05 14:47:05 +0300

    A very rough movement script, will improve later

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public int speed = 60;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3();
+        if(Input.GetKey("w"))
+            move += transform.forward;
+        if(Input.GetKey("s"))
+            move += -transform.forward;
+        if(Input.GetKey("a"))
+            move += -transform.right;
+        if(Input.GetKey("d"))
+            move += transform.right;
+        transform.Translate(move/(100-speed));
+    }
+}
